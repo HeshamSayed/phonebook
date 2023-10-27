8cebc1222279b7bce4bc9f84c62d6c740f22cc14
heshamsayed <heshamsayed@paymob.com>
2023-10-27 18:16:33 +0200

    adding login required mixin for detailview

--- contacts/views/contact_detail_view.py
+++ contacts/views/contact_detail_view.py
@@ -1,9 +1,9 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.detail import DetailView
-
 from ..models.contact_model import Contact
 
 
-class ContactDetailView(DetailView):
+class ContactDetailView(LoginRequiredMixin, DetailView):
     model = Contact
     template_name = 'contact_detail.html'
     context_object_name = 'contact'
