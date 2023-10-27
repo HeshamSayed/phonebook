85612c2d9caa73072a4900b5a317d5ac11b8a219
heshamsayed <heshamsayed@paymob.com>
2023-10-27 18:13:33 +0200

    add login required mixin for ContactListView

--- contacts/views/contact_list_view.py
+++ contacts/views/contact_list_view.py
@@ -1,7 +1,10 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.list import ListView
+
 from ..models.contact_model import Contact
 
-class ContactListView(ListView):
+
+class ContactListView(LoginRequiredMixin, ListView):
     model = Contact
     template_name = 'contact_list.html'
     context_object_name = 'contacts'
